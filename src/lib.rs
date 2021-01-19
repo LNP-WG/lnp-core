41c86fb463c6524c56e8bfa2d51d9200c37cdd15
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-01-19 23:19:09 +0100

    Refactoring into LNP Core Lib

--- src/lib.rs
+++ src/lib.rs
@@ -14,32 +14,27 @@
 #![recursion_limit = "256"]
 // Coding conventions
 #![deny(
-    non_upper_case_globals,
-    non_camel_case_types,
-    non_snake_case,
-    unused_mut,
-    unused_imports,
-    dead_code,
-    //missing_docs
+non_upper_case_globals,
+non_camel_case_types,
+non_snake_case,
+unused_mut,
+unused_imports,
+dead_code,
+//missing_docs
 )]
-// TODO: when we will be ready for the release #![deny(missing_docs)]
-// This is required because of incomplete rust async implementation and can be
-// removed after async trait feature completion in rust compiler
-#![cfg_attr(feature = "async", allow(where_clauses_object_safety))]
 
 #[macro_use]
 extern crate amplify;
 #[macro_use]
 extern crate amplify_derive;
 #[macro_use]
-extern crate lazy_static;
-
-extern crate chacha20poly1305;
-
-// Support for node & node clients development (include API helpers)
-#[cfg(feature = "async")]
+extern crate strict_encoding;
 #[macro_use]
-extern crate async_trait;
+extern crate lazy_static;
+#[macro_use]
+extern crate num_derive;
+#[macro_use]
+extern crate bitcoin_hashes;
 
 #[cfg(feature = "serde")]
 #[macro_use]
@@ -47,64 +42,24 @@ extern crate serde_with;
 #[cfg(feature = "serde")]
 extern crate serde_crate as serde;
 
-// Bitcoin-specific imports. We make them public while we use custom versions
-// of the libs so downstream dependencies can use them directly from this lib
-// TODO: Refactor re-exported bitcoin and hashes functionality
-pub extern crate bitcoin;
-pub use bitcoin::secp256k1;
-#[macro_use]
-pub extern crate bitcoin_hashes as hashes;
-pub use hashes::hex;
-pub extern crate miniscript;
-#[cfg(feature = "bulletproofs")]
-pub extern crate secp256k1zkp;
+pub mod channel;
+pub mod extension;
+pub mod factories;
+mod features;
+pub mod message;
+pub mod payment;
+pub mod peer_connection;
+pub mod prometheus;
+pub mod storm;
 
-pub extern crate client_side_validation;
-pub use client_side_validation::commit_encode_list;
-pub use client_side_validation::commit_verify;
-#[macro_use]
-pub extern crate strict_encoding;
-pub use strict_encoding::{
-    impl_enum_strict_encoding, strict_decode_self, strict_encode_list,
-    test_encode, test_enum_u8_exhaustive, test_garbage_exhaustive,
+pub use extension::{
+    ChannelExtension, Extension, GossipExtension, RoutingExtension,
 };
-#[macro_use]
-pub extern crate strict_encoding_derive;
-pub use strict_encoding_derive::{StrictDecode, StrictEncode};
-
-pub mod chain;
-pub mod dbc;
-#[cfg(feature = "elgamal")]
-pub mod elgamal;
-pub mod seals;
-pub mod short_id;
-pub mod tagged_hash;
-
-#[macro_use]
-pub mod test_helpers;
-#[macro_use]
-mod paradigms;
-mod standards;
-#[macro_use]
-pub mod bp;
-#[cfg(feature = "lnp")]
-#[allow(dead_code, unused_variables)]
-// TODO: Remove attribute once LNP mod will be finalized
-pub mod lnp;
-
-pub use lnp::presentation::encoding as lightning_encoding;
-pub use paradigms::{
-    client_side_validation, commit_verify, single_use_seals, strict_encoding,
+pub use features::{FeatureContext, FeatureFlag, Features};
+pub use message::{Messages, OnionPacket, LNPWP_UNMARSHALLER};
+pub use payment::{ChannelId, TempChannelId};
+pub use peer_connection::{
+    PeerConnection, PeerReceiver, PeerSender, RecvMessage, SendMessage,
 };
-#[cfg(feature = "elgamal")]
-pub use standards::elgamal;
-pub use standards::{features, lnpbp1, lnpbp2, lnpbp3, lnpbp4};
 
-lazy_static! {
-    /// Global Secp256k1 context object
-    pub static ref SECP256K1: bitcoin::secp256k1::Secp256k1<bitcoin::secp256k1::All> =
-        bitcoin::secp256k1::Secp256k1::new();
-
-    pub static ref SECP256K1_PUBKEY_DUMB: bitcoin::secp256k1::PublicKey =
-        bitcoin::secp256k1::PublicKey::from_secret_key(&SECP256K1, &bitcoin::secp256k1::key::ONE_KEY);
-}
+pub const LIGHTNING_P2P_DEFAULT_PORT: u16 = 9735;
