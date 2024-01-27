6a4f8799616a32daa6504bcfd1ab61e51cd6d92d
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-27 09:10:37 -0300

    Update bolt1.rs

--- lnp2p/src/bolt/bolt1.rs
+++ lnp2p/src/bolt/bolt1.rs
@@ -1,7 +1,7 @@
 // LNP P2P library, plmeneting both bolt (BOLT) and Bifrost P2P messaging
 // system for Lightning network protocol (LNP)
 //
-// Written in 2020-2022 by
+// Written in 2020-2024 by
 //     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
 //
 // To the extent possible under law, the author(s) have dedicated all
