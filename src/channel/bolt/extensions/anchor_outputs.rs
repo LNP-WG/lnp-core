452bcce4461140e1113e70564d3d2a3969dc9d76
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-01-27 08:58:14 -0300

    Update anchor_outputs.rs

--- src/channel/bolt/extensions/anchor_outputs.rs
+++ src/channel/bolt/extensions/anchor_outputs.rs
@@ -1,5 +1,5 @@
 // LNP/BP Core Library implementing LNPBP specifications & standards
-// Written in 2020-2022 by
+// Written in 2020-2024 by
 //     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
 //
 // To the extent possible under law, the author(s) have dedicated all
